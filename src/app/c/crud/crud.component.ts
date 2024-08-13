538a4925e45627f45caf382cf5f225205cc83670
Sourabh Rana <75777528+Sourabhrana98@users.noreply.github.com>
2024-08-13 15:14:59 +0530

    commit

---
+++ src/app/c/crud/crud.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+interface Item {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-crud',
+  templateUrl: './crud.component.html',
+  styleUrls: ['./crud.component.css']
+})
+export class CrudComponent {
+  items: Item[] = [];
+
+  newItem: string = '';
+  editItemName: string = '';
+  editItemId: number | null = null;
+
+  createItem() {
+    if (this.newItem) {
+      const newId = this.items.length + 1;
+      this.items.push({ id: newId, name: this.newItem });
+      this.newItem = '';
+    }
+  }
+  
+
+  editItem(item: Item) {
+    this.editItemName = item.name;
+    this.editItemId = item.id;
+  }
+
+  updateItem() {
+    if (this.editItemId !== null) {
+      const item = this.items.find(i => i.id === this.editItemId);
+      if (item) {
+        item.name = this.editItemName;
+        this.editItemName = '';
+        this.editItemId = null;
+      }
+    }
+  }
+
+  deleteItem(id: number) {
+    this.items = this.items.filter(item => item.id !== id);
+  }
+}
